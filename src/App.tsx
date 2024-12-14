50591205a1e0c14c4dcd493e7240fda26d4f198e
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-12-14 13:06:43 -0500

    feat: add initial setup with Tailwind CSS, utility functions, and UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { useFieldArray, useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "./components/ui/button";
+
+const itemObject = z.object({
+  name: z.string().nonempty("Name is required"),
+  price: z.preprocess(
+    (val) =>
+      val === "" || val === undefined ? undefined : parseFloat(val as string),
+    z
+      .number({
+        required_error: "Price is required",
+        invalid_type_error: "Price must be a number",
+      })
+      .min(0, "Price cannot be less than 0")
+  ),
+});
+
+const formSchema = z.object({
+  items: z.array(itemObject),
+  tax: z
+    .preprocess((val) => parseFloat(val as string), z.number().optional())
+    .optional(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      items: [{ name: "", price: 0 }],
+      tax: 0,
+    },
+    mode: "onBlur",
+    reValidateMode: "onChange",
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "items",
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log("Submitted data:", values);
+  };
+
+  const canRemove = fields.length > 1;
+
+  const highlightText = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.select();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>splittr</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex gap-4 items-center">
+              <FormField
+                control={form.control}
+                name={`items.${index}.name`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Item Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name={`items.${index}.price`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        onFocus={highlightText}
+                        placeholder="Item Price"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {canRemove && (
+                <Button
+                  type="button"
+                  variant="destructive"
+                  onClick={() => remove(index)}
+                >
+                  Remove
+                </Button>
+              )}
+            </div>
+          ))}
+          <div className="mt-4">
+            <Button
+              type="button"
+              onClick={() => append({ name: "", price: 0 })}
+            >
+              Add Item
+            </Button>
+            <Button type="submit" className="ml-2">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/item.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const Item = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("space-y-2", className)} {...props} />;
+});
+
+export { Item };
